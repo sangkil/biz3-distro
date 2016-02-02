7d138ff17425a89f843af9275981f5ad2ac4c5a4
mujib.masyhudi <mujib.masyhudi@gmail.com>
2016-02-02 10:32:30 +0700

    set theming

---
+++ backend/models/master/Orgn.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace backend\models\master;
+
+use Yii;
+
+/**
+ * This is the model class for table "orgn".
+ *
+ * @property integer $id
+ * @property string $code
+ * @property string $name
+ * @property integer $created_at
+ * @property integer $created_by
+ * @property integer $updated_at
+ * @property integer $updated_by
+ *
+ * @property Branch[] $branches
+ */
+class Orgn extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'orgn';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
+            [['code'], 'string', 'max' => 4],
+            [['name'], 'string', 'max' => 32],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'code' => 'Code',
+            'name' => 'Name',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+            'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBranches()
+    {
+        return $this->hasMany(Branch::className(), ['orgn_id' => 'id']);
+    }
+}
