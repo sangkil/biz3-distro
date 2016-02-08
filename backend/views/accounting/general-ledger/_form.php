e8a9ae63ace58224adb7131410797b52423d38d2
mujib.masyhudi <mujib.masyhudi@gmail.com>
2016-02-08 09:07:08 +0700

    acc start.

--- backend/views/accounting/general-ledger/_form.php
+++ backend/views/accounting/general-ledger/_form.php
@@ -15,9 +15,8 @@ use backend\models\master\Branch;
     <div class="col-lg-4">
         <?= $form->field($model, 'number')->textInput(['maxlength' => true, 'style' => 'width:30%;']) ?>
         <?= $form->field($model, 'branch_id')->dropDownList(Branch::selectOptions(), ['style' => 'width:40%;']) ?>
-        <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
     </div>
-    <div class="col-lg-8">      
+    <div class="col-lg-4">      
         <?=
         $form->field($model, 'GlDate')->widget('yii\jui\DatePicker', [
             'dateFormat' => 'dd-MM-yyyy',
@@ -25,7 +24,9 @@ use backend\models\master\Branch;
         ])
         ?>
         <?= $form->field($model, 'periode_id')->dropDownList(backend\models\accounting\AccPeriode::selectOptions(), ['style' => 'width:40%;']) ?>
-        <?= $form->field($model, 'status')->textInput(['style' => 'width:60%;']) ?>
+    </div>
+    <div class="col-lg-4">      
+        <?= $form->field($model, 'description')->textarea(['maxlength' => true]) ?>
     </div>
 
     <div class="col-lg-12 form-group">
