4afd4185260b03a8a0ece69f389e73f58f2100b7
mujib <mujib@sangkilsoft>
2016-03-04 09:46:07 +0700

    some sales update

--- backend/views/sales/sales/_form.php
+++ backend/views/sales/sales/_form.php
@@ -16,7 +16,7 @@ JuiAsset::register($this);
 $opts = json_encode([
     'product_url' => Url::to(['product-list']),
     'vendor_url' => Url::to(['vendor-list']),
-        ]);
+    ]);
 
 $this->registerJs("var biz = $opts;", View::POS_HEAD);
 $this->registerJs($this->render('_script.js'));
@@ -44,8 +44,10 @@ $this->registerJs($this->render('_script.js'));
     <div class="col-md-4">        
         <div class="small-box bg-aqua box-comments">
             <div class="inner">
-                <h3>Rp0</h3>
-                <p>0 Items</p>
+                <h3><span id="sales-value-text">Rp0</span>&nbsp;</h3>
+                <p><span id="sales-qty-text">0 Items</span></p>
+                <input type="hidden" id="sales-value">
+                <input type="hidden" id="sales-qty">
             </div>
             <div class="icon">
                 <i class="fa fa-shopping-cart"></i>
@@ -57,11 +59,20 @@ $this->registerJs($this->render('_script.js'));
         <div class="box box-info box-comments with-border">
             <div class="box-header with-border">
                 <i class="fa fa-shopping-cart text-orange"></i>
-                <h3 class="box-title">Customer & Payments</h3>   
+                <h3 class="box-title">Casier : <?= (Yii::$app->user->isGuest) ? 'Guest' : Yii::$app->user->identity->username ?></h3>
                 <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse">
-                        <i class="fa fa-minus"></i>
-                    </button>
+                    <div class="button-group">
+                        <button type="button" class="btn btn-box-tool dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                            <i class="fa fa-gear"></i></button>
+                        <ul class="dropdown-menu" role="menu">
+                            <div class="col-lg-12">
+                                <li>Active Branch</li>
+                                <li>Warehouse</li>
+                                <li class="divider"></li>
+                                <li>Something else here</li>
+                            </div>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="box-body with-border">
@@ -71,24 +82,46 @@ $this->registerJs($this->render('_script.js'));
                                 </div>-->
                 <div class="col-lg-12">
                     <?= $form->field($model, 'vendor_name')->textInput([])->label('Customer') ?>
-                    <?= $form->field($model, 'vendor_id')->hiddenInput()->label(false) ?>
+                    <?= $form->field($model, 'vendor_id')->hiddenInput()->label(false) ?>  
                 </div>
-                <div class="col-lg-6">
-                    <?= $form->field($payment, 'payment_method')->dropDownList($payment::enums('METHOD_', ['prompt' => '--'])) ?>
-                </div>
-                <div class="col-lg-4">
-                    <?= Html::label('Value') ?>
-                    <?= Html::textInput('payment[items][value]', '', ['class' => 'form-control']) ?>
-                </div>
-                <div class="col-lg-2" style="padding-left: 0px;">
-                    <?= Html::buttonInput('Add', ['class' => 'btn btn-primary', 'style' => 'margin-top:24px;']) ?>
+                <div id="payment-form" class="hidden">
+                    <?= \yii\bootstrap\Html::hiddenInput('payment-value', 0, ['id' => 'payment-value']) ?>
+                    <div class="col-lg-12">
+                        <table class="grid-view hidden" id="payment-grid">
+                            <thead>
+                                <tr>
+                                    <th>Payment Type</th>
+                                    <th>Value</th>
+                                    <th>Remain</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Cash</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-lg-6">
+                        <?= $form->field($payment, 'payment_method')->dropDownList($payment::enums('METHOD_', ['prompt' => '--'])) ?>
+                    </div>
+                    <div class="col-lg-4">
+                        <?= Html::label('Value') ?>
+                        <?= Html::textInput('payment[items][value]', '', ['class' => 'form-control','id'=>'payment-items-value']) ?>
+                    </div>
+                    <div class="col-lg-2" style="padding-left: 0px;">
+                        <?= Html::buttonInput('Add', ['class' => 'btn btn-primary', 'style' => 'margin-top:24px;', 'id'=>'payment-add']) ?>
+                    </div>
                 </div>
             </div>
-            <div class="box-footer box-comments">
+            <div class="box-footer box-comments hidden" id="payment-completion">
                 <div class="col-lg-12">
-                <?=
-                Html::submitButton($model->isNewRecord ? 'Complete' : '', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary'])
-                ?></div>
+                    <?=
+                    Html::submitButton($model->isNewRecord ? 'Complete' : '', ['class' => $model->isNewRecord ? 'btn btn-success'
+                                : 'btn btn-primary'])
+                    ?></div>
             </div>
         </div>
     </div>
