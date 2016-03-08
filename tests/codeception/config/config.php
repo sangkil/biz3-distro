6abecfff986a0137aa53c6e8588f2bc8eb908454
mdmunir <misbahuldmunir@gmail.com>
2016-03-08 11:01:47 +0700

    tests config

--- tests/codeception/config/config.php
+++ tests/codeception/config/config.php
@@ -14,7 +14,7 @@ return [
     ],
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2_advanced_tests',
+            'dsn' => 'sqlite:' . dirname(__DIR__) . '/_output/test.sqlite',
         ],
         'mailer' => [
             'useFileTransport' => true,
