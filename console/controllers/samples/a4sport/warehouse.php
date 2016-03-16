6f25c1f03c91c72e4cdb440df5cd3d4768aaef9e
mujib <mujib@sangkilsoft>
2016-03-17 06:35:58 +0700

    reset data-sample

--- console/controllers/samples/a4sport/warehouse.php
+++ console/controllers/samples/a4sport/warehouse.php
@@ -1,7 +1,7 @@
 <?php
 return[
-    [1, 1, "1101", "Whse Lubeg"],
+    [1, 1, "1101", "Whse Pusat"],
     [2, 2, "1201", "Whse Permindo"],
-    [3, 3, "1301", "Whse Permindo pusat"],
+    [3, 3, "1301", "Whse Lubeg"],
     [4, 4, "1401", "Whse Bukittinggi"],
 ];
