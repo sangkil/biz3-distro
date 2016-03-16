163881652dbba7a6658c7f500df9ab1ee5877d2e
mujib <mujib@sangkilsoft>
2016-03-16 18:27:08 +0700

    reset data-sample

---
+++ console/controllers/samples/a4sport/branch.php
@@ -0,0 +1,7 @@
+<?php
+return[
+    [1, 1, "1100", "Adidas Permindo Pusat"],
+    [2, 1, "1100", "Adidas Permindo"],
+    [3, 1, "1200", "A4Sport Lubeg"],
+    [4, 1, "1300", "Adidas Bukittinggi"],
+];
