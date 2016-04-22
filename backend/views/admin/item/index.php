fc17ac17aac7bbbb26a2e7281a7c72395e4977fc
mujib <mujib@sangkilsoft>
2016-04-22 11:23:40 +0700

    update menu

---
+++ backend/views/admin/item/index.php
@@ -0,0 +1,52 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use mdm\admin\components\RouteRule;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $searchModel mdm\admin\models\searchs\AuthItem */
+/* @var $context mdm\admin\components\ItemController */
+
+$context = $this->context;
+$labels = $context->labels();
+$this->title = Yii::t('rbac-admin', $labels['Items']);
+$this->params['breadcrumbs'][] = $this->title;
+
+$rules = array_keys(Yii::$app->getAuthManager()->getRules());
+$rules = array_combine($rules, $rules);
+unset($rules[RouteRule::RULE_NAME]);
+?>
+<div class="role-index">
+    <p class="pull-right">
+        <?= Html::a(Yii::t('rbac-admin', 'Create ' . $labels['Item']), ['create'], ['class' => 'btn btn-success']) ?>
+
+    </p><br>
+
+    <?=
+    GridView::widget([
+        'dataProvider' => $dataProvider,
+        'tableOptions' => ['class' => 'table table-hover'],
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            [
+                'attribute' => 'name',
+                'label' => Yii::t('rbac-admin', 'Name'),
+            ],
+            [
+                'attribute' => 'ruleName',
+                'label' => Yii::t('rbac-admin', 'Rule Name'),
+                'filter' => $rules
+            ],
+            [
+                'attribute' => 'description',
+                'label' => Yii::t('rbac-admin', 'Description'),
+            ],
+            ['class' => 'yii\grid\ActionColumn',],
+        ],
+    ])
+    ?>
+
+</div>
