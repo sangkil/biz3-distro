cba5b903dd302682381dce63f6d49899497c1295
mujib <mujib@sangkilsoft>
2016-04-25 12:43:44 +0700

    report menu update

--- backend/views/report/report/persediaan.php
+++ backend/views/report/report/persediaan.php
@@ -4,4 +4,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo 'persediaan';
+?>
+<div class="col-lg-6">
+    <h2>Stock</h2>
+    <p></p>
+    <p><a class="btn btn-default" href="#">Get Started &raquo;</a></p>
+</div>
+<div class="col-lg-6">
+    <h2>Stock History</h2>
+    <p></p>
+    <p><a class="btn btn-default" href="#">Get Started &raquo;</a></p>
+</div>
--- backend/views/report/report/sales.php
+++ backend/views/report/report/sales.php
@@ -4,4 +4,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo 'sales';
+?>
+<div class="col-lg-6">
+    <h2>Sales Harian</h2>
+    <p></p>
+    <p><a class="btn btn-default" href="#">Get Started &raquo;</a></p>
+</div>
+<div class="col-lg-6">
+    <h2>Rekap Bulanan</h2>
+    <p></p>
+    <p><a class="btn btn-default" href="#">Get Started &raquo;</a></p>
+</div>
