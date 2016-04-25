6a70504175b8830ccaa9da7ed0e9f461e10e68d5
mujib <mujib@sangkilsoft>
2016-04-25 12:51:02 +0700

    report menu update

--- backend/views/report/report/persediaan.php
+++ backend/views/report/report/persediaan.php
@@ -15,3 +15,8 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
     <p><a class="btn btn-default" href="#">Get Started &raquo;</a></p>
 </div>
+<div class="col-lg-6">
+    <h2>Stock Adjustment</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    <p><a class="btn btn-default" href="#">Get Started &raquo;</a></p>
+</div>
