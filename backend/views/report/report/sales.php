6d6a29a2364ab17fcdad893478eecd7de2d98d2a
mujib <mujib@sangkilsoft>
2016-04-25 12:48:50 +0700

    report menu update

--- backend/views/report/report/sales.php
+++ backend/views/report/report/sales.php
@@ -7,11 +7,11 @@
 ?>
 <div class="col-lg-6">
     <h2>Sales Harian</h2>
-    <p></p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
     <p><a class="btn btn-default" href="#">Get Started &raquo;</a></p>
 </div>
 <div class="col-lg-6">
     <h2>Rekap Bulanan</h2>
-    <p></p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
     <p><a class="btn btn-default" href="#">Get Started &raquo;</a></p>
 </div>
