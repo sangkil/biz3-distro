575d3c3be7eae77c84d643a82e2fc3e87577a4a7
mujib <mujib@sangkilsoft>
2016-04-26 18:19:52 +0700

    Endonesa

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -3,21 +3,42 @@
 
 $this->title = 'SangkilBiz3-Distro';
 ?>
-<div class="site-index">
+<div class="row site-index">
+    <div class="col-md-6">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="http://placehold.it/900x300/39CCCC/ffffff&amp;text=A4Sport+Lubeg" alt="First slide">
+                    <div class="carousel-caption">
+                        Outlet 1
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="http://placehold.it/900x300/3c8dbc/ffffff&amp;text=Adidas+Permindo" alt="Second slide">
 
-    <div class="jumbotron">
-        <h1>A4Sport Apps</h1>
+                    <div class="carousel-caption">
+                        Outlet 2
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="http://placehold.it/900x300/f39c12/ffffff&amp;text=Adidas+Bukittinggi" alt="Third slide">
 
-        <p class="lead">Yii-powered bussines management application</p>
-
-        <!--<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>-->
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-           
+                    <div class="carousel-caption">
+                        Outlet 3
+                    </div>
+                </div>
+            </div>
+            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                <span class="fa fa-angle-left"></span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                <span class="fa fa-angle-right"></span>
+            </a>
         </div>
-
     </div>
 </div>
