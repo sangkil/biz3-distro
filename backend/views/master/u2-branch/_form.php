18ae216c60d81e51bdba2b178a5473f50fe0f0fa
mujib.masyhudi <mujib.masyhudi@gmail.com>
2016-05-17 16:16:15 +0700

    skin for production

--- backend/views/master/u2-branch/_form.php
+++ backend/views/master/u2-branch/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\web\JsExpression;
+use mdm\admin\models\User;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\master\U2Branch */
@@ -12,9 +14,28 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'branch_id')->dropDownList(\backend\models\master\Branch::selectOptions(),['style'=>'width:60%;'])->label('Branch') ?>
+    <?= $form->field($model, 'branch_id')->dropDownList(\backend\models\master\Branch::selectOptions(), ['style' => 'width:60%;'])->label('Branch') ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
+    <?php
+    $data = User::find()
+            ->select(['id', 'username'])
+            ->asArray()
+            ->all();
+
+    echo AutoComplete::widget([
+        'name' => 'nm_user',
+        'id' => 'dnm_user',
+        'clientOptions' => [
+            'source' => $data,
+            'autoFill' => true,
+            'minLength' => '1',
+            'select' => new JsExpression("function( event, ui ) {
+                alert(ui.item.id);
+             }")],
+    ]);
+    ?>
+
+    <?= $form->field($model, 'user_id')->hiddenInput()->label(false) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
