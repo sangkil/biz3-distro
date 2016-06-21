265cf7df02ba42302157dc16da1a7e3707cff98b
mujib.masyhudi <mujib.masyhudi@gmail.com>
2016-06-21 09:16:21 +0700

    dashboard

--- backend/views/site/_dashTransfer.php
+++ backend/views/site/_dashTransfer.php
@@ -8,8 +8,11 @@
 <?php //if($model->sisa>0){ ?>
 <li class="item" style="<?php if($index>10000){ ?>border-top: 1px whitesmoke solid;<?php } ?> padding: 10px;">
     <div class="product-info">
-        <?= yii\helpers\Html::a($model->number, ['/inventory/transfer/view','id'=>$model->id]) ?>
-        <span class="label label-success pull-right"><?= ''//number_format($model->sisa,0) ?> </span>
+        <label>
+            <?= yii\helpers\Html::a($model->number, ['/inventory/transfer/view','id'=>$model->id]) ?>
+            <?= $model->Date ?>
+        </label>        
+<!--        <span class="label label-success pull-right"><?= ''//number_format($model->sisa,0) ?> </span>-->
         <span class="product-description">
             <?= 'Source: '.$model->branch->name ?>
             <?= 'Destination: '.$model->branchDest->name .'<br>'?>
