4d2f002d7bedb0859a1b9cd71bcd227144bfcb58
mujib.masyhudi <mujib.masyhudi@gmail.com>
2016-06-27 13:56:20 +0700

    dashboard enhance

--- backend/views/site/dashboard.php
+++ backend/views/site/dashboard.php
@@ -15,6 +15,37 @@ $this->title = 'Dashboard ' . $mperiode;
     ]);
     ?> 
     <div class="col-lg-12"></div>
+    <!--Transfer Stock-->
+    <div class="col-lg-6">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Stock Transfer</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <ul class="products-list product-list-in-box">
+                    <?=
+                    ListView::widget([
+                        'dataProvider' => $transfPro,
+                        'layout' => "{items}",
+                        'itemView' => '_dashTransfer',
+                        'emptyText' => '&nbsp;'
+                    ]);
+                    ?>                    
+                </ul>
+            </div>
+            <!-- /.box-body
+            <div class="box-footer text-center">
+                <a href="javascript:void(0)" class="uppercase">View All Products</a>
+            </div>
+            /.box-footer -->
+        </div>
+    </div>
     <!--    Hutang Dagang-->
     <div class="col-lg-6">
         <div class="box box-primary">
@@ -47,35 +78,4 @@ $this->title = 'Dashboard ' . $mperiode;
         </div>
     </div>
 
-    <!--Transfer Stock-->
-    <div class="col-lg-6">
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <h3 class="box-title">Stock Transfer</h3>
-                <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                </div>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <ul class="products-list product-list-in-box">
-                    <?=
-                    ListView::widget([
-                        'dataProvider' => $transfPro,
-                        'layout' => "{items}",
-                        'itemView' => '_dashTransfer',
-                        'emptyText' => '&nbsp;'
-                    ]);
-                    ?>                    
-                </ul>
-            </div>
-            <!-- /.box-body
-            <div class="box-footer text-center">
-                <a href="javascript:void(0)" class="uppercase">View All Products</a>
-            </div>
-            /.box-footer -->
-        </div>
-    </div>
 </div>
