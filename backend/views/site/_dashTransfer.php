2911c45fbdb2e97d699913a244c67ecf34799af8
mujib.masyhudi <mujib.masyhudi@gmail.com>
2016-06-27 13:36:17 +0700

    dashboard enhance

--- backend/views/site/_dashTransfer.php
+++ backend/views/site/_dashTransfer.php
@@ -6,21 +6,23 @@
 ?>
 <!-- /.item -->
 <?php //if($model->sisa>0){ ?>
-<li class="item" style="<?php if($index>10000){ ?>border-top: 1px whitesmoke solid;<?php } ?> padding: 10px;">
+<li class="item" style="<?php if ($index > 10000) { ?>border-top: 1px whitesmoke solid;<?php } ?> padding: 10px;">
     <div class="product-info">
         <label>
-            <?= yii\helpers\Html::a($model->number, ['/inventory/transfer/view','id'=>$model->id]) ?>
-            <?= '/&nbsp;'.$model->Date ?>
+            <?= yii\helpers\Html::a($model->number, ['/inventory/transfer/view', 'id' => $model->id]) ?>
+            <?= '/&nbsp;' . $model->Date ?>
         </label>        
-<!--        <span class="label label-success pull-right"><?= ''//number_format($model->sisa,0) ?> </span>-->
+<!--        <span class="label label-success pull-right"><?= ''//number_format($model->sisa,0)  ?> </span>-->
         <span class="product-description">
-            <?= 'Source: '.$model->branch->name ?>
-            <?= 'Destination: '.$model->branchDest->name .'<br>'?>
-            <?php foreach ($model->movements as $dmove){
-                echo '&nbsp;'.\yii\helpers\Html::a($dmove->number,['/inventory/gm-manual/view','id'=>$dmove->id]);
-            } ?>
+            <?= 'Source: ' . $model->branch->name ?>
+            <?= 'Destination: ' . $model->branchDest->name . '<br>' ?>
+            <?php
+            foreach ($model->movements as $dmove) {
+                echo ($dmove->status == backend\models\inventory\GoodsMovement::STATUS_CANCELED) ? '' : '&nbsp;' . \yii\helpers\Html::a($dmove->number, ['/inventory/gm-manual/view', 'id' => $dmove->id]);
+            }
+            ?>
         </span>
     </div>
 </li>
-<?php //} ?>
+<?php //}  ?>
 <!-- /.item -->
