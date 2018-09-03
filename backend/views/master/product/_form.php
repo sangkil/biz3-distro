f5cfc8f45ca06bbf1e9cfbc020789c0250f3b0c6
mujib masyhudi <mujib.masyhudi@gmail.com>
2018-09-03 09:50:33 +0700

    #change code length to 16

--- backend/views/master/product/_form.php
+++ backend/views/master/product/_form.php
@@ -14,7 +14,7 @@ use yii\bootstrap\ActiveForm;
     ?>
     <div class="row">
         <div class="col-lg-4">
-            <?= $form->field($model, 'code')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'code')->textInput(['maxlength' => true, 'style' => 'width:50%']) ?>
             <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
         </div>
         <div  class="col-lg-4">
@@ -29,7 +29,7 @@ use yii\bootstrap\ActiveForm;
             ])
             ?>
             <?= $form->field($model, 'stockable')->checkbox() ?>
-<?= $form->field($model, 'status')->dropDownList($model::enums('STATUS_'), ['prompt' => '-- select status--', 'style' => 'width:40%']) ?>
+            <?= $form->field($model, 'status')->dropDownList($model::enums('STATUS_'), ['prompt' => '-- select status--', 'style' => 'width:40%']) ?>
         </div>
 
         <div class="nav-tabs-justified col-lg-12"  style="margin-top: 20px;">
@@ -38,21 +38,21 @@ use yii\bootstrap\ActiveForm;
                 <li><a href="#bcode" data-toggle="tab" aria-expanded="false">Barcodes Alias</a></li>    
                 <li><a href="#dprice" data-toggle="tab" aria-expanded="false">Sales Price</a></li>      
                 <li class="pull-right">    
-<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+                    <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
                 </li>             
             </ul> 
             <div class="tab-content" >
                 <div class="tab-pane active" id="uom">
-<?= $this->render('_form-uom', ['product_uom' => $model->productUoms]) ?>
+                    <?= $this->render('_form-uom', ['product_uom' => $model->productUoms]) ?>
                 </div>
                 <div class="tab-pane" id="bcode">
-<?= $this->render('_form-barcode', ['product_bcode' => $model->productChildren]) ?>
+                    <?= $this->render('_form-barcode', ['product_bcode' => $model->productChildren]) ?>
                 </div>
                 <div class="tab-pane" id="dprice">                    
-<?= $this->render('_form-price', ['product_prices' => $model->prices]) ?>
+                    <?= $this->render('_form-price', ['product_prices' => $model->prices]) ?>
                 </div>
             </div> 
         </div>
     </div>
-<?php ActiveForm::end(); ?>
+    <?php ActiveForm::end(); ?>
 </div>
