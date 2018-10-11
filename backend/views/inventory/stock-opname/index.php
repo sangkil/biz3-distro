38553e328698aadca8d30a7bee517b8ac4178769
mujib masyhudi <mujib.masyhudi@gmail.com>
2018-10-11 12:13:48 +0700

    #partial opname

--- backend/views/inventory/stock-opname/index.php
+++ backend/views/inventory/stock-opname/index.php
@@ -43,7 +43,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'operator',
             [
                 'attribute' => 'type',
-                'value' => 'type',
+                'value' => 'nmType',
                 'filter' => StockOpname::enums('TYPE_')
             ],
             [
