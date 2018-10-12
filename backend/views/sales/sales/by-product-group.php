844ade5253d55f4e4979dc662189c733996f9cd3
mujib masyhudi <mujib.masyhudi@gmail.com>
2018-10-12 15:26:19 +0700

    #sales with principal grouped

--- backend/views/sales/sales/by-product-group.php
+++ backend/views/sales/sales/by-product-group.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <p class="pull-right">
     <?=
-    Html::a('<i class="fa fa-download"></i>', ['by-product-month-csv', 'SalesDtl[FrDate]' => $searchModel->FrDate, 'SalesDtl[ToDate]' => $searchModel->ToDate, 'SalesDtl[branch_id]'=>$searchModel->branch_id], [
+    Html::a('<i class="fa fa-download"></i>', ['by-product-month-csv', 'SalesDtl[FrDate]' => $searchModel->FrDate, 'SalesDtl[ToDate]' => $searchModel->ToDate, 'SalesDtl[branch_id]' => $searchModel->branch_id], [
         'class' => 'btn btn-default', 'title' => 'CSV Download', 'target' => '_blank',
         'data' => [
             'method' => 'post',
@@ -23,35 +23,37 @@ $this->params['breadcrumbs'][] = $this->title;
 </p><br>
 <div class="sales-dtl-index"> 
     <?php echo $this->render('_searchdtl', ['model' => $searchModel]); ?> 
-    <?php
-    $content = Html::beginTag('table', ['class' => 'table']);
-    $hdr = ['No', 'Group Name',  'Sales Value', 'Diskon'];
-    $hdrw = [5, 10, 10, 0, 10, 5, 5, 10, 10, 10];
-    $content .= Html::beginTag('tr');
-    $j = 0;
-    foreach ($hdr as $value) {
-        $algn = (in_array($j, [7, 8, 9])) ? 'right;' : 'left;';
-        $content .= ($j == 3) ? Html::tag('th', $value) : Html::tag('th', $value, ['style' => 'width:' . $hdrw[$j] . '%; text-align:' . $algn]); //['style'=>'text-align:right;']
-        $j++;
-    }
-    $content .= Html::endTag('tr');
-
-    $dbfore = '';
-    $is_first = true;
-    $i = 0;
-    foreach ($dataProvider->models as $row) {
+    <div class="col-lg-6">
+        <?php
+        $content = Html::beginTag('table', ['class' => 'table']);
+        $hdr = ['No', 'Group Name', 'Sales Value', 'Diskon'];
+        $hdrw = [5, 10, 10, 0, 10, 5, 5, 10, 10, 10];
         $content .= Html::beginTag('tr');
-        $content .= Html::tag('td', $row->group_name); //category
-        $content .= Html::tag('td', number_format($row->amount, 0), ['style' => 'text-align:right;']);
-        $content .= Html::tag('td', number_format($row->disc, 0), ['style' => 'text-align:right;']);
-        $content .= Html::tag('td', number_format($row->amount - $row->disc, 0), ['style' => 'text-align:right;']);
+        $j = 0;
+        foreach ($hdr as $value) {
+            $algn = (in_array($j, [7, 8, 9])) ? 'right;' : 'left;';
+            $content .= ($j == 3) ? Html::tag('th', $value) : Html::tag('th', $value, ['style' => 'width:' . $hdrw[$j] . '%; text-align:' . $algn]); //['style'=>'text-align:right;']
+            $j++;
+        }
         $content .= Html::endTag('tr');
 
-        $dbfore = $row->SDate;
-        $is_first = false;
-        $i++;
-    }
-    $content .= Html::endTag('table');
-    echo $content;
-    ?> 
+        $dbfore = '';
+        $is_first = true;
+        $i = 0;
+        foreach ($dataProvider->models as $row) {
+            $content .= Html::beginTag('tr');
+            $content .= Html::tag('td', $row->group_name); //category
+            $content .= Html::tag('td', number_format($row->amount, 0), ['style' => 'text-align:right;']);
+            $content .= Html::tag('td', number_format($row->disc, 0), ['style' => 'text-align:right;']);
+            $content .= Html::tag('td', number_format($row->amount - $row->disc, 0), ['style' => 'text-align:right;']);
+            $content .= Html::endTag('tr');
+
+            $dbfore = $row->SDate;
+            $is_first = false;
+            $i++;
+        }
+        $content .= Html::endTag('table');
+        echo $content;
+        ?> 
+    </div>
 </div> 
